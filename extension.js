d021ac0cd5fbcf6aa98ec4a963803f11e85ea9e0
Francisco Rubin Capalbo <pancho_111203@hotmail.com>
2018-04-20 11:11:28 +0200

    Fixed unicode error in windows #1

--- extension.js
+++ extension.js
@@ -60,7 +60,7 @@ function activate(context) {
             endLine = editor.selection.end.line + 1;
         }
 
-        sendQueuedText(`__file__ = '${filename}'`);
+        sendQueuedText(`__file__ = r'${filename}'`);
         sendQueuedText('sys.path.append(os.path.dirname(__file__))')
         const command = `%load -r ${startLine}-${endLine} ${filename}`;
         sendQueuedText(command);
@@ -75,7 +75,7 @@ function activate(context) {
 
         const editor = vscode.window.activeTextEditor;
         const filename = editor.document.fileName;
-        sendQueuedText(`__file__ = '${filename}'`);
+        sendQueuedText(`__file__ = r'${filename}'`);
         sendQueuedText('sys.path.append(os.path.dirname(__file__))')
         sendQueuedText(`%load ${filename}`, 100);
         sendQueuedText('\n\n');
